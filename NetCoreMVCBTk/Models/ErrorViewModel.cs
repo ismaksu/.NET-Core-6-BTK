3067329bf80d3b0e3b151479d228025a5701574a
ismaksu <mcpebagimli@gmail.com>
2023-09-10 16:43:34 +0300

    Initial commit.

---
+++ NetCoreMVCBTk/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NetCoreMVCBTk.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
